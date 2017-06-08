3a98079fe7727d0855ba0df5856b12d9205b306b
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2017-06-08 13:15:32 +0900

    lec 02 up

---
+++ ThreadingTest/2_Thread2_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*]
+[Background 쓰레드 vs Foreground 쓰레드 ]
+
+Thread 클래스 객체를 생성한 후 Start()를 실행하기 전에 IsBackground 속성을 true/false로 지정할 수 있는데, 
+만약 true로 지정하면 이 쓰레드는 백그라운드 쓰레드가 된다. 
+디폴트 값은 false 즉 Foreground 쓰레드이다. 
+백그라운드와 Foreground 쓰레드의 기본적인 차이점은 
+Foreground 쓰레드는 메인 쓰레드가 종료되더라도 Foreground 쓰레드가 살아 있는 한, 프로세스가 종료되지 않고 계속 실행되고, 
+백그라운드 쓰레드는 메인 쓰레드가 종료되면 바로 프로세스를 종료한다는 점이다. 
+*/
+namespace _2_Thread2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Foreground 쓰레드
+            Thread t1 = new Thread(new ThreadStart(Run));
+            t1.Start();
+
+            Thread t2 = new Thread(new ThreadStart(Run));
+            t2.IsBackground = true;
+            t2.Start();
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+
+        private static void Run()
+        {
+            Console.WriteLine("Run");
+        }
+    }
+}
--- ThreadingTest/Thread1/Program.cs
+++ ThreadingTest/Thread1/Program.cs
@@ -11,6 +11,8 @@ Thread클래스 객체에서 Start() 메서드를 호출하면 새로운 쓰레�
 아래 예는 동일 클래스 안의 Run() 메서드를 실행하는 쓰레드를 하나 생성한 후 실행시키는 예제이다. 
 예제에서는 기본적으로 생성된 메인 쓰레드에서도 동일하게 Run()메서드를 호출하고 있으므로, 
 Begin/End문장이 2번 출력되고 있는데, 이는 2개의 쓰레드가 동시에 한 메서드를 실행하고 있기 때문이다. 
+
+※출처 : http://www.csharpstudy.com/Threads/thread.aspx
 */
 namespace Thread1
 {
